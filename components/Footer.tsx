00e197a1b4ceeeea7b1278c6d7d91df9cbec157b
Mr-Archisman <1807327@kiit.ac.in>
2025-05-03 17:44:07 +0530

    hello@sick.design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -80,7 +80,7 @@ export default function Footer() {
 								E:
 							</h1>
 							<LinkHover
-								title="hello@ochi.design"
+								title="hello@sick.design"
 								href="/"
 								className="before:h-[1px] after:h-[1px] paragraph font-medium before:bottom-[-3px] after:bottom-[-3px]"
 							/>
