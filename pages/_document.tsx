2e753b1da337863fc370c166e4c29d9b7880013e
Mr-Archisman <1807327@kiit.ac.in>
2025-05-03 17:46:41 +0530

    fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>OCHI - Presentation Design Agency</title>
+      <title>SICK STUDIOS - Design Agency</title>
       <Head />
       <body>
         <Main />
